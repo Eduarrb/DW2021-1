2f6d58a51ea8fe0d11801935b701c04de6ef9e0f
Eduardo Arroyo <dj.shadowjeab@gmail.com>
2021-01-28 18:02:27 -0500

    clase 12

--- 03-CREATIVE/js/codigo.js
+++ 03-CREATIVE/js/codigo.js
@@ -2,17 +2,21 @@
 
 // console.log("12346846452");
 
+// DOM -> DOCUMENT OBJECT MODEL
+
 var nav = document.querySelector('.nav');
 
 window.addEventListener('scroll', movimientoScroll);
 
 function movimientoScroll(){
     // console.log('hiciste scroll');
-    console.log(window.pageYOffset);
+    // console.log(window.pageYOffset);
     if(window.pageYOffset > 0){
-        console.log('es mayor a cero');
+        // console.log('es mayor a cero');
+        nav.classList.add('active');
     }
     else{
-        console.log('es igual a cero');
+        // console.log('es igual a cero');
+        nav.classList.remove('active');
     }
 }
